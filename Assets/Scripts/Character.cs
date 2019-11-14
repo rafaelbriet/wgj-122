0be5401f338f06b22dad8a824c02e9ad5ca461cf
Rafael Briet <rafael-briet@hotmail.com>
2019-11-13 23:34:32 -0300

    Character randomizer

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+	private Material[] materials;
+
+	[SerializeField] private Color[] clothColors;
+	[SerializeField] private Color[] hairColors;
+	[SerializeField] private Color[] skinColors;
+
+    void Start()
+    {
+		materials = GetComponentInChildren<Renderer>().materials;
+
+		materials[0].SetColor("_Color", clothColors[Random.Range(0, clothColors.Length)]);
+		materials[1].SetColor("_Color", skinColors[Random.Range(0, skinColors.Length)]);
+		materials[4].SetColor("_Color", hairColors[Random.Range(0, hairColors.Length)]);
+    }
+}
